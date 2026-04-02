a6ca5c74c86f74777afe72e5905676a53cc036fa
Lonnex <njengalonnex340@gmail.com>
2026-04-02 10:45:12 +0300

    chore: updated dependencies for prisma downgrade to v6 and database schema for verification workflow. updated the prisma.ts to ensure the stable singleton pattern is used.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,27 +1,19 @@
 //lib/prisma.ts
 //Prisma client singleton for Next.js to prevent multiple instances during development
 
+// ---> Explained <---//
+//nn Next.js, the server reloads every time u save a file. Without the globalForPrisma check in the code, prisma create a new connection to the db every time i save, eventually causeing db crash with a "too many connections" error
+
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-//Create PostgreSQL connection pool
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-})
-
-//create Prisma adapter
-const adapter = new PrismaPg(pool)
-
 export const prisma =
     globalForPrisma.prisma ??
     new PrismaClient({
-        adapter,
-        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'], //querry logging
+        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
     })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
